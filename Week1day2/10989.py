e6c4d16f5ad6f80f63b2bdd0eeec902571800275
knamk7 <knamk7@gmail.com>
2023-03-24 01:21:23 +0900

    0323:[10989, O]

--- Week1day2/10989.py
+++ Week1day2/10989.py
@@ -1,9 +1,11 @@
+import sys
+
 N = int(input())
-li = [0]*10001
-element = 1
+li = [0]*10001 # Instead of storing values directly in the list, store the number of each value.
+element = 0 # For fewer computation, one more element is declared in the list. Original code had 10000 elements in the list.
 
 for i in range(N):
-    li[int(input())] += 1
+    li[int(sys.stdin.readline())] += 1 # It is faster than normal input(). Deleted +1 by adding index of 0 into the list.
 
 for i in li:
     for j in range(i):
