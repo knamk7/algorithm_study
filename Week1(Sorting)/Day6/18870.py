04fa33e9b8cd10ac84bf2d15d5b813d08679c9d3
knamk7 <knamk7@gmail.com>
2023-03-29 01:18:34 +0900

    0328:[18870, O]

--- Week1(Sorting)/Day6/18870.py
+++ Week1(Sorting)/Day6/18870.py
@@ -1,15 +1,10 @@
 N = int(input())
 li1 = list(map(int, input().split()))
-li2 = sorted((set(li1)))
-li3 = [0] * N
-k = 0
+li2 = sorted(set(li1))
+dic = dict() # O(1) for getting values by keys
 
-for i in li2:
-    while i in li1:
-        temp = li1.index(i)
-        li3[temp] = k
-        li1[temp] = 10000000000
-    k += 1
+for i in range(len(li2)):
+    dic[li2[i]] = i
 
-for i in li3:
-    print(i, end=' ')
+for i in li1:
+    print(dic[i], end=' ')
