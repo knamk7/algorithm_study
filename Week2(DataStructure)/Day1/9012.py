45e548f2a2afc69f88dd452ecee2098f55dcbc33
knamk7 <knamk7@gmail.com>
2023-03-30 15:54:09 +0900

    0330:[10845, O]

--- Week2(DataStructure)/Day1/9012.py
+++ Week2(DataStructure)/Day1/9012.py
@@ -1,7 +1,9 @@
-T = int(input())
+import sys
+
+T = int(sys.stdin.readline().strip())
 string = list()
 for i in range(T):
-    string.append(list(input()))
+    string.append(list(sys.stdin.readline().strip()))
 for i in string:
     a = 0
     for j in range(len(i)):
@@ -9,9 +11,7 @@ for i in string:
         if temp == ')': a += 1
         else: a -= 1
         if a < 0: break
-    if a < 0:
-        print('NO')
-    elif a == 0:
+    if a == 0:
         print('YES')
-    elif a != 0:
+    else:
         print('NO')
---
+++ Week2(DataStructure)/Day2/10845.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline().strip())
+commands = list()
+q = deque()
+
+for i in range(N):
+    commands.append(list(sys.stdin.readline().strip().split()))
+
+for i in commands:
+    if i[0] == 'push':
+        q.append(int(i[1]))
+    elif i[0] == 'pop':
+        try:
+            print(q.popleft())
+        except:
+            print('-1')
+    elif i[0] == 'size':
+        print(len(q))
+    elif i[0] == 'empty':
+        if len(q) == 0:
+            print('1')
+        else:
+            print('0')
+    elif i[0] == 'front':
+        if len(q) == 0:
+            print('-1')
+        else:
+            print(q[0])
+    elif i[0] == 'back':
+        if len(q) == 0:
+            print('-1')
+        else:
+            print(q[-1])
