c0ed526dd75f5dee1104368971ecfbf3e45c70e6
knamk7 <knamk7@gmail.com>
2023-04-05 00:38:42 +0900

    0404:[2075, O]

--- Week2(DataStructure)/Day6/2075.py
+++ Week2(DataStructure)/Day6/2075.py
@@ -1,20 +1,12 @@
-# 못 푼 문제
-
 import sys
-import heapq as h
+import heapq
 
 N = int(input())
-data = []
+data = list(map(int, sys.stdin.readline().strip().split()))
 
-for i in range(N-5):
-    sys.stdin.readline()
-for i in range(5): # Only last 5 lines matter
+for i in range(N-1):
     temp = list(map(int, sys.stdin.readline().strip().split()))
     for j in temp:
-        h.heappush(data, (-j, j))
+        heapq.heappushpop(data, j)
 
-h.heappop(data)
-h.heappop(data)
-h.heappop(data)
-h.heappop(data)
-print(h.heappop(data)[1])
+print(heapq.heappop(data))
