6e1764aa5ba3a07448d001c6741ed49a70320094
knamk7 <knamk7@gmail.com>
2023-05-14 02:54:58 +0900

    0513:[1874, O]

--- Week2(DataStructure)/Day5/1874.py
+++ Week2(DataStructure)/Day5/1874.py
@@ -1,25 +1,24 @@
-# 못 푼 문제
-# stack 마지막 값이 target에 있으면 pop
-# 없으면 stack에 자연수를 오름차순으로 push
-# target이 안만들어지면 print('NO')
-
 import sys
 
 N = int(input())
-target = list()
-stack = []
-result = list()
+target = []
+stack = [0]
+result = []
 
 for i in range(N):
     target.append(int(sys.stdin.readline().strip()))
 
-temp = 1
+temp = 0
 for i in target:
-    stack.append(temp)
     while i > stack[-1]:
         temp += 1
         stack.append(temp)
         result.append('+')
+    if stack.pop() == i:
+        result.append('-')
+    else:
+        result = ['NO']
+        break
 
 for i in result:
     print(i)
